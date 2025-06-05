b7dd5fb63db1766297f0ff5a7476a7960573a3de
Chris Wilson <github@yepher.com>
2025-06-05 13:02:39 -0500

    adds components-swift

---
+++ livekit/components-swift/Sources/LiveKitComponents/Scopes/RoomScope.swift
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2025 LiveKit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import LiveKit
+import SwiftUI
+
+public struct RoomScope<Content: View>: View {
+    private let _content: () -> Content
+    @StateObject private var _room: Room
+
+    private let _url: String?
+    private let _token: String?
+    private let _connect: Bool
+
+    private let _enableCamera: Bool
+    private let _enableMicrophone: Bool
+
+    public init(room: Room? = nil,
+                url: String? = nil,
+                token: String? = nil,
+                connect: Bool = true,
+                enableCamera: Bool = false,
+                enableMicrophone: Bool = false,
+                roomOptions: RoomOptions? = nil,
+                @ViewBuilder _ content: @escaping () -> Content)
+    {
+        __room = StateObject(wrappedValue: room ?? Room(roomOptions: roomOptions))
+        _url = url
+        _token = token
+        _connect = connect
+        _enableCamera = enableCamera
+        _enableMicrophone = enableMicrophone
+        _content = content
+    }
+
+    public var body: some View {
+        _content()
+            .environmentObject(_room)
+            .onAppear {
+                if _connect, let url = _url, let token = _token {
+                    Task {
+                        try await _room.connect(url: url, token: token)
+                        if _enableCamera { try await _room.localParticipant.setCamera(enabled: true) }
+                        if _enableMicrophone { try await _room.localParticipant.setMicrophone(enabled: true) }
+                    }
+                }
+            }
+            .onDisappear {
+                Task {
+                    await _room.disconnect()
+                }
+            }
+    }
+}
---
+++ livekit/components-swift/Sources/LiveKitComponents/UI/LocalCameraPreview.swift
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2025 LiveKit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import LiveKit
+import SwiftUI
+
+public struct LocalCameraPreview: View {
+    private let _localVideoTrack: LocalVideoTrack
+
+    @Environment(\.liveKitUIOptions) var ui: UIOptions
+
+    public init(localVideoTrack: LocalVideoTrack? = nil) {
+        _localVideoTrack = localVideoTrack ?? LocalVideoTrack.createCameraTrack()
+    }
+
+    public var body: some View {
+        GeometryReader { geometry in
+
+            ZStack {
+                ui.videoDisabledView(geometry: geometry)
+
+                SwiftUIVideoView(_localVideoTrack,
+                                 mirrorMode: .mirror)
+                    .onAppear {
+                        Task {
+                            try await _localVideoTrack.start()
+                        }
+                    }
+                    .onDisappear {
+                        Task {
+                            try await _localVideoTrack.stop()
+                        }
+                    }
+            }
+        }
+    }
+}
